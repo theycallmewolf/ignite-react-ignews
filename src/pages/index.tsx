95540f331a2baf099a66fa3fc919a8d6d3d22476
theycallmewolf <bruno.m.lobato@gmail.com>
2021-05-07 15:47:07 +0100

    v.0.0.1 moves title from _document into home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
 import styles from '../styles/home.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
-  return <h1 className={styles.title}>Hello Wolf!</h1>;
+  return (
+    <>
+      <Head>
+        <title>home | ig.news</title>
+        <meta name="description" content="..." />
+      </Head>
+      <h1 className={styles.title}>Hello Wolf!</h1>
+    </>
+  );
 }
