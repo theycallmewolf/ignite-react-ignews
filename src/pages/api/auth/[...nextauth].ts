1d8335a503bb38689389acfdee0e37d619a075e8
theycallmewolf <bruno.m.lobato@gmail.com>
2021-05-10 17:44:48 +0100

    v.0.0.1 saves user on db after login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import {query as q} from 'faunadb';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -10,4 +12,37 @@ export default NextAuth({
       // @see: https://docs.github.com/en/developers/apps/scopes-for-oauth-apps
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  }
 })
