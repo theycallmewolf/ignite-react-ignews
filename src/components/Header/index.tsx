9022f0e8a27ca5a62afdd3c9f4b88a055af849a9
theycallmewolf <bruno.m.lobato@gmail.com>
2021-05-17 21:17:45 +0100

    v.0.0.1 adds home link to header logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,16 @@
+import Link from 'next/link';
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+
 export function Header () {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="logo ig.news" />
+        <Link href="/">
+          <a>
+            <img src="/images/logo.svg" alt="logo ig.news" />
+          </a>
+        </Link>
         <nav>
           <a href="" className={styles.active}>Home</a>
           <a href="">Posts</a>
