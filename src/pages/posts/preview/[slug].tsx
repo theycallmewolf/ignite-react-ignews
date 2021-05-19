c7e5459d923fbe531023722593aee13554cacbc6
theycallmewolf <bruno.m.lobato@gmail.com>
2021-05-19 16:36:08 +0100

    v.0.0.1 adds post preview page

---
+++ src/pages/posts/preview/[slug].tsx
@@ -0,0 +1,66 @@
+import Head from 'next/head';
+import { GetStaticProps } from "next";
+import { RichText } from "prismic-dom";
+import { getPrismicClient } from "../../../services/prismic";
+import styles from '../post.module.scss'
+
+interface PostPreviewProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  }
+}
+
+export default function PostPreview({ post }: PostPreviewProps) {
+  return (
+    <>
+      <Head>
+        <title>{post.title} | ignews</title>
+      </Head>
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div 
+            dangerouslySetInnerHTML={{__html: post.content}}
+            className={styles.content}
+          />
+        </article>
+      </main>
+    </>
+  );
+}
+
+export const getStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps : GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+  
+  const prismic = getPrismicClient();
+
+  const response = await prismic.getByUID('post', String(slug), {});
+
+  const post = {
+    slug, 
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-PT', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    })
+  }
+
+  return {
+    props: {
+      post,
+    }
+  }
+}
