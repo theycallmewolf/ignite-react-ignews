999f89e9eb0fef571f4cd9b2e02d2d4952895b67
theycallmewolf <bruno.m.lobato@gmail.com>
2021-05-19 15:29:59 +0100

    v.0.0.1 adds session callback to nextAuth w/ subscription details

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,6 +13,34 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    async session(session) {
+      const userActiveSubscription = await fauna.query(
+        q.Get(
+          q.Intersection([                            // other options: Difference, Union
+            q.Match(
+              q.Index('subscription_by_user_ref'),
+              q.Select(                               // choose dataset
+                'ref',
+                q.Get(                                // get data
+                  q.Match(                            // find record
+                    q.Index('user_by_email'),
+                    q.Casefold(session.user.email)
+                  )
+                )
+              )
+            ),
+            q.Match(
+              q.Index('subscription_by_status'),
+              'active'
+            )
+          ])
+        )
+      )
+      return {
+        ...session,
+        activeSubscription: userActiveSubscription,
+      };
+    },
     async signIn(user, account, profile) {
       const { email } = user;
 
