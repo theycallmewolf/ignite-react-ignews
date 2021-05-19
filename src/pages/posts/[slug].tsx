2eec72fcdcb5e37d1f2bcedde287bebbe360f94d
theycallmewolf <bruno.m.lobato@gmail.com>
2021-05-19 14:32:45 +0100

    v.0.0.1 adds post page

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,46 @@
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/client";
+import { RichText } from "prismic-dom";
+import { getPrismicClient } from "../../services/prismic";
+
+interface PostProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  }
+}
+
+export default function Post({ post }: PostProps) {
+  return (
+    <h1>Test</h1>
+  );
+}
+
+export const getServerSideProps : GetServerSideProps = async ({ req, params }) => {
+  const session = await getSession({ req });
+  const { slug } = params;
+  // if(!session) {}
+
+  const prismic = getPrismicClient(req);
+
+  const response = await prismic.getByUID('publication', String(slug), {});
+
+  const post = {
+    slug, 
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-PT', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    })
+  }
+
+  return {
+    props: {
+      post,
+    }
+  }
+}
